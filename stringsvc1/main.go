d35b98fa64a1f4a89625240ba6ba6f23c2e8fac0
Thomas Scholtz <thomasscholtz@Thomass-MacBook-Pro.local>
2023-06-06 09:57:35 +0200

    revisit stringsvc1: more commenting and error handling suggestions

--- stringsvc1/main.go
+++ stringsvc1/main.go
@@ -24,6 +24,9 @@ type stringService struct{}
 // ErrEmpty is returned when an input string is empty.
 var ErrEmpty = errors.New("empty string")
 
+// Malformed Request is returned when the endpoint-specific type assertion on the arbitrary request interface{} fails
+var MalformedRequest = errors.New("type assertion of request interface failed")
+
 // Implementation methods of StringService via stringService struct
 func (stringService) Uppercase(s string) (string, error) {
 	if s == "" {
@@ -64,18 +67,29 @@ type countResponse struct {
 
 // Endpoint is the fundamental building block of servers and clients.
 // It represents a single RPC method.
+// All methods in a service interface are converted to endpoints,
+// Essentially, this conversion from method to endpoint just implies that method of an interface is executed in a configured scope i.e.,
+// service interface is passed into make<method>Endpoint func to create a closure where that interface is available.
+// make<method>Endpoint then returns a func that ALWAYS accepts: context and an arbitrary request interface (to be the corresponding method's request struct),
+// and ALWAYS returns: arbitrary response interface (to be corresponding method's response struct) and an error.
+// within the first-class function that is returned, the service interface simply applies its method to the accepted request to create a response, and returns it.
+// now, a method on an interface is primed to become an endpoint on an API
 func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
-	// Essentially, the endpoint is a function that takes context and any request type
+	// The endpoint is a function that takes context and any request type
 	// and returns any response type and an error
 	// Recall: the empty interface can hold any type because all types implement at least no methods
 
 	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(uppercaseRequest) // For the Uppercase endpoint, we know we require an uppercaseRequest from our request interface{}
+		req, ok := request.(uppercaseRequest) // For the Uppercase endpoint, we know we require an uppercaseRequest from our request interface{}
+		if !ok {
+			return uppercaseResponse{"", MalformedRequest.Error()}, MalformedRequest
+		}
 		// The above operation is a type assertion, it will extract the uppercaseRequest from the empty interface
+		// I recommend the ok as without it, failed type assertions cause a panic
 		v, err := svc.Uppercase(req.S) // Insert the S field of the request into the Uppercase implementation
 		// Return the response of the service's method, depending on err
 		if err != nil {
-			return uppercaseResponse{v, err.Error()}, nil // Notice the error is contained in the uppercaseResponse struct
+			return uppercaseResponse{v, err.Error()}, nil // Notice the error is contained in the uppercaseResponse struct - but why is error nil here? that's misleading
 		}
 		return uppercaseResponse{v, ""}, nil
 	}
